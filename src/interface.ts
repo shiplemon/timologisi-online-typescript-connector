7722109331d75714534ed1171b7f860a2a106349
 Stratos Giouldasis <giouldasis.stratos@gmail.com>
2021-07-16 22:50:02 +0300

    Exports every tinybit of interface

--- src/interface.ts
+++ src/interface.ts
@@ -1,64 +1,102 @@
+export interface paymentMethodDetails {
+  type?: string;
+  amount?: string;
+  paymentMethodInfo?: string;
+}
+
+export interface paymentMethods {
+  paymentMethodDetails?: paymentMethodDetails;
+}
+
+export interface invoiceHeader {
+  series: string;
+  aa?: string;
+  issueDate?: string;
+  invoiceType: string;
+  vatPaymentSuspension: string;
+  currency: string;
+}
+
+export interface invoiceSummary {
+  totalNetValue: string;
+  totalVatAmount: string;
+  totalWithheldAmount: string;
+  totalFeesAmount: string;
+  totalStampDutyAmount: string;
+  totalOtherTaxesAmount: string;
+  totalDeductionsAmount: string;
+  totalGrossValue: string;
+}
+
+export interface invoiceDetail {
+  lineNumber?: string;
+  lineCode: string;
+  measurementUnitLabel?: string;
+  lineUnitPrice: string;
+  totalNetPriceBeforeDiscount: string;
+  totalDiscountValue: string;
+  netValue: string;
+  vatCategory?: string;
+  vatCategoryPercent?: string;
+  vatAmount: string;
+  deductionsAmount: string;
+  lineComments: string;
+  lineDescription: string;
+}
+
+export type invoiceDetails = invoiceDetail[];
+
+export interface API_Counterpart {
+  CounterpartCode?: string;
+  CounterpartName?: string;
+  CounterpartProfession?: string;
+  CounterpartTaxoffice?: string;
+  CounterpartAddressStreet?: string;
+  CounterpartAddressNumber?: string;
+  CounterpartAddressPostalCode?: string;
+  CounterpartAddressCity?: string;
+  CounterpartAddressCountry?: string;
+  CounterpartPhone?: string;
+  CounterpartEmail?: string;
+}
+
+export interface API_Additionals {
+  DocumentLabel?: string;
+  DocumentComments?: string;
+  paymentMethodInvoiceLabel?: string;
+}
+
+export interface API_InvoiceDetails {
+  API_Counterpart?: API_Counterpart;
+  API_Additionals?: API_Additionals;
+}
+
+export interface Issuer {
+  vatNumber: string;
+  branch?: string;
+  country: string;
+
+  name: string;
+  profession: string;
+  taxoffice: string;
+  addressStreet: string;
+  addressNumber: string;
+  addressPostalCode: string;
+  addressCity: string;
+  addressCountry: string;
+  phone: string;
+  fax?: string;
+  email: string;
+  webSite: string;
+  registryNumber: string;
+}
+
 export interface InvoiceRequest {
-  paymentMethods?: {
-    paymentMethodDetails?: {
-      type?: string;
-      amount?: string;
-      paymentMethodInfo?: string;
-    };
-  };
-  invoiceHeader: {
-    series: string;
-    aa?: string;
-    issueDate?: string;
-    invoiceType: string;
-    vatPaymentSuspension: string;
-    currency: string;
-  };
-  invoiceSummary?: {
-    totalNetValue: string;
-    totalVatAmount: string;
-    totalWithheldAmount: string;
-    totalFeesAmount: string;
-    totalStampDutyAmount: string;
-    totalOtherTaxesAmount: string;
-    totalDeductionsAmount: string;
-    totalGrossValue: string;
-  };
-  invoiceDetails: {
-    lineNumber?: string;
-    lineCode: string;
-    measurementUnitLabel?: string;
-    lineUnitPrice: string;
-    totalNetPriceBeforeDiscount: string;
-    totalDiscountValue: string;
-    netValue: string;
-    vatCategory?: string;
-    vatCategoryPercent?: string;
-    vatAmount: string;
-    deductionsAmount: string;
-    lineComments: string;
-    lineDescription: string;
-  }[];
-  API_InvoiceDetails?: {
-    API_Counterpart?: {
-      CounterpartCode?: string;
-      CounterpartName?: string;
-      CounterpartProfession?: string;
-      CounterpartTaxoffice?: string;
-      CounterpartAddressStreet?: string;
-      CounterpartAddressNumber?: string;
-      CounterpartAddressPostalCode?: string;
-      CounterpartAddressCity?: string;
-      CounterpartAddressCountry?: string;
-      CounterpartPhone?: string;
-      CounterpartEmail?: string;
-    };
-    API_Additionals?: {
-      DocumentLabel?: string;
-      DocumentComments?: string;
-      paymentMethodInvoiceLabel?: string;
-    };
-  };
+  paymentMethods?: paymentMethods;
+  invoiceHeader: invoiceHeader;
+  invoiceSummary?: invoiceSummary;
+  invoiceDetails: invoiceDetails;
+  API_InvoiceDetails?: API_InvoiceDetails;
 }
 
 export interface InvoiceResponse {
@@ -74,23 +112,5 @@ export interface InvoiceResponse {
 export interface InvoiceOptions {
   sandbox: boolean;
   apiKey: string;
-  issuer: {
-    vatNumber: string;
-    branch?: string;
-    country: string;
-
-    name: string;
-    profession: string;
-    taxoffice: string;
-    addressStreet: string;
-    addressNumber: string;
-    addressPostalCode: string;
-    addressCity: string;
-    addressCountry: string;
-    phone: string;
-    fax?: string;
-    email: string;
-    webSite: string;
-    registryNumber: string;
-  };
+  issuer: Issuer;
 }
